807ad93eb33c2edd379c1337fe6526a1d0a718d3
antonio.rehwinkel@barkhauseninstitut.org <antonio.rehwinkel@barkhauseninstitut.org>
2024-03-07 18:00:16 +0100

    colorfull ^^

--- paging-demo/src/app/users/PageTableView.tsx
+++ paging-demo/src/app/users/PageTableView.tsx
@@ -11,9 +11,10 @@ interface props {
 function PageTableView(props: props) {
   const pageDirectory = props.pageDirectory;
   const pageTables = props.pageTables;
+
   return (
-    <>
-      <div>
+    <div className=" p-2 flex flex-row">
+      <div className="p-2 border border-red-900">
         <TableRenderer
           tableName="Page Dir"
           table={pageDirectory.table}
@@ -21,15 +22,29 @@ function PageTableView(props: props) {
         />
       </div>
 
-      <div>
-        <div className="table table-bordered w-auto">
-          <h1> PageTables</h1>
-          {pageTables.map((table) => (
-            <TableRenderer tableName="" table={table.table} markedId={5} />
-          ))}
-        </div>
+      <div className="pl-10 border border-sky-800">
+        <table className="table table-auto table-bordered w-auto">
+          <thead>
+            <tr>
+              <th>PageTables</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              {pageTables.map((table) => (
+                <td>
+                  <TableRenderer
+                    tableName={table.table[0][0].toString()}
+                    table={table.table}
+                    markedId={5}
+                  />
+                </td>
+              ))}
+            </tr>
+          </tbody>
+        </table>
       </div>
-    </>
+    </div>
   );
 }
 
--- paging-demo/src/app/users/TableRenderer.tsx
+++ paging-demo/src/app/users/TableRenderer.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 interface table {
   tableName: string;
   table: number[][];
